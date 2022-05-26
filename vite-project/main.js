e7c25ca626a13c6df2c3bd264f7ee3761ca7db11
DerrickChen28 <90790119+DerrickChen28@users.noreply.github.com>
2022-05-26 04:39:17 -0400

    fjr3fr

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,12 @@
 import './style.css'
+const DOMSelectors = {
+    input1: document.getElementById('title'),
+};
+
+document.getElementById("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    erase();
+});
+function erase () {
+    DOMSelectors.input1.value = " ";
+}
